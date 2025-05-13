02b0ef16ccb5ba8d499f15c6b01595c6ac45e06a
UnaiT <u_telleriamad@fpzornotza.com>
2025-05-13 10:20:33 +0200

    EXPORTAR SESIONES XML

--- src/main/LogronoAPP.java
+++ src/main/LogronoAPP.java
@@ -10,8 +10,15 @@ import java.sql.DriverManager;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -23,38 +30,72 @@ import model.Sesion;
 
 public class LogronoAPP {
 	
-	public static void main(String[] args) throws SQLException
+	public static void main(String[] args) throws SQLException, ParserConfigurationException, TransformerException
 	{
 		
-		importarXML();
-		/*ArrayList<Empleado> empleados = new ArrayList<>();
-
-		Connect con = new Connect();
-		Administrador admin = new Administrador();
-		Empleado empleado = new Empleado();
-		con.cargarEmpleado(empleados);
-		
-		for(int i = 0;i<con.cargarEmpleado().size();i++) {					
-			System.out.println(con.cargarEmpleado().get(i).getDNI());		
-		};
-		for(int i = 0;i<con.cargarEmpleado().size();i++) {					
-				System.out.println(con.cargarEmpleado().get(i).getDNI());		
-			};
-
-*/
+		//importarXML();
+		//exportarXML();
 	}
 	
+	public static void exportarXML() throws ParserConfigurationException, TransformerException, SQLException {
+		 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder builder = factory.newDocumentBuilder();
+
+	        Connect conexion = new Connect();
+	        Connection con = conexion.conexion();
+	  
+	        Statement st = con.createStatement();
+	    	String consulta = "SELECT * FROM sesion";
+	    	ResultSet rs = st.executeQuery(consulta);
+	    	
+	    	
+	       // Create a new Document
+	        Document document = builder.newDocument();
+	        
+	        // Create root element
+	        Element root = document.createElement("sesion");
+	        document.appendChild(root);
+	        
+			while(rs.next()) {
+	        // Create book elements and add text content
+	        Element codigo = document.createElement("Codigo");
+	        codigo.appendChild(document.createTextNode(rs.getString("Codigo")));
+	        Element fecha = document.createElement("Fecha");
+	        fecha.appendChild(document.createTextNode(rs.getString("Fecha")));
+	        Element aforo = document.createElement("Aforo");
+	        aforo.appendChild(document.createTextNode(rs.getString("Aforo")));
+	        Element actividad = document.createElement("Actividad_Codigo");
+	        actividad.appendChild(document.createTextNode(rs.getString("Actividad_Codigo")));
+	        root.appendChild(codigo);
+	        root.appendChild(fecha);
+	        root.appendChild(aforo);
+	        root.appendChild(actividad);
+			}
+			
+	        // Write to XML file
+	        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	        Transformer transformer = transformerFactory.newTransformer();
+	        DOMSource source = new DOMSource(document);
+
+	        // Specify your local file path
+	        StreamResult result = new StreamResult("C:\\Users\\1AW3-24\\git\\Logro-o_deportes_grupo2\\src\\main\\XML\\sesiones.xml");
+	        transformer.transform(source, result);
+
+	        System.out.println("XML file created successfully!");
+	}
+
+	
 	public static void importarXML()  throws SQLException{
-DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-Document documento = null;
-try
-{
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		Document documento = null;
+	try
+		{
 	Connect conexion = new Connect();
     Connection con = conexion.conexion();
 	DocumentBuilder builder = factory.newDocumentBuilder(); 
-	documento = builder.parse( new File("C:/Users/1AW3-24/git/Logro-o_deportes_grupo2/src/main/pruebaReto.xml") );
+	documento = builder.parse( new File("C:/Users/1AW3-24/git/Logro-o_deportes_grupo2/src/main/XML/pruebaReto.xml") );
 	NodeList nodeList = documento.getElementsByTagName("reserva");
-    for (int i = 0; i < nodeList.getLength(); i = i+2) {
+    for (int i = 0; i < nodeList.getLength(); i = i++) {
         Node node = nodeList.item(i);
         String reserva = node.getTextContent();
         String[] datos = reserva.split("\n");
